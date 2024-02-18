32783c07778fa070b4941977b62caa0c42a73e7b
BunnyWarlock <93790489+BunnyWarlock@users.noreply.github.com>
2024-02-18 20:09:17 +0600

    Update Convex Hull.cpp

--- Algorithms/Geometry/Convex Hull.cpp
+++ Algorithms/Geometry/Convex Hull.cpp
@@ -16,10 +16,10 @@ vector<pll> convexHull(vector<pll> pts) {
 	if (pts.size() <= 1) return pts;
 	sort(pts.begin(), pts.end());
 	vector<pll> h(pts.size()+1);
-  auto cross = [](pll p, pll a, pll b){
-    return (a.first-p.first)*(b.second-p.second) -
-	  			(a.second-p.second)*(b.first-p.first);
-  }; // Returns the cross product of vectors pa and pb
+	auto cross = [](pll p, pll a, pll b){
+	return (a.first-p.first)*(b.second-p.second) -
+				(a.second-p.second)*(b.first-p.first);
+	}; // Returns the cross product of vectors pa and pb
 	int s = 0, t = 0, i;
 	for (i = 2; i--; s = --t, reverse(pts.begin(), pts.end()))
 		for (pll& p : pts) {
