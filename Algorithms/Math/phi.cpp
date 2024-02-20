a734581036e73cb19cdd05da31e9ad9e74348162
BunnyWarlock <93790489+BunnyWarlock@users.noreply.github.com>
2024-02-20 15:08:37 +0600

    Create phi.cpp

---
+++ Algorithms/Math/phi.cpp
@@ -0,0 +1,48 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <numeric>
+using namespace std;
+#define endl '\n'
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template<class T>
+// using oset = tree<T, null_type, less<T>, rb_tree_tag,
+//              tree_order_statistics_node_update>;
+
+template <class ...Args> auto &print(const Args &...args)
+{ return ((cerr<<args<<", "), ...)<<"\b\b)"<<endl; }
+#define watch(...) if(true){cerr<<"("#__VA_ARGS__") = ("; print(__VA_ARGS__);}
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define read(arr) for(auto& x: arr) cin>>x
+#define write(arr) for(auto& x: arr) cout<<x<<" "; cout<<endl
+// #define mem(x, n) memset(x, n, sizeof(x))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+const ll MAX = 1e5;
+const ll MOD = 1e9+7;
+int phi[MAX];
+void calcPhi(){
+  iota(phi, phi+MAX, 0);
+  for (int i = 2; i < MAX; ++i)
+    if (phi[i] == i)
+      for (int j = i; j < MAX; j += i)
+        phi[j] -= phi[j]/i;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+
+    
+
+    return 0;
+}
