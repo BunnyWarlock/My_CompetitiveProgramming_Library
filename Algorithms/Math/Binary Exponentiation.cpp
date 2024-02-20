1c98b1e3601475339b02388d3b6b58f5d2f94b97
BunnyWarlock <93790489+BunnyWarlock@users.noreply.github.com>
2024-02-20 15:06:57 +0600

    Create Binary Exponentiation.cpp

---
+++ Algorithms/Math/Binary Exponentiation.cpp
@@ -0,0 +1,50 @@
+// #include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <numeric>
+using namespace std;
+#define endl '\n'
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+// template<class T>
+// using oset = tree<T, null_type, less<T>, rb_tree_tag,
+//              tree_order_statistics_node_update>;
+
+template <class ...Args> auto &print(const Args &...args)
+{ return ((cerr<<args<<", "), ...)<<"\b\b)"<<endl; }
+#define watch(...) if(true){cerr<<"("#__VA_ARGS__") = ("; print(__VA_ARGS__);}
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define read(arr) for(auto& x: arr) cin>>x
+#define write(arr) for(auto& x: arr) cout<<x<<" "; cout<<endl
+// #define mem(x, n) memset(x, n, sizeof(x))
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+
+ll powMod(ll x, ll p, ll m){
+  ll ans = 1;
+  x %= m;
+  if (!x) return 0;
+  while(p){
+    if (p&1)
+      ans = (ans*x)%m;
+    p = p>>1;
+    x = (x*x)%m;
+  }
+  return ans;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin.exceptions(cin.failbit);
+
+    
+
+    return 0;
+}
