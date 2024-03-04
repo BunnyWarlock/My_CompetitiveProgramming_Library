fb062b655df9b2ae50d246d79889752e71fb4c10
BunnyWarlock <93790489+BunnyWarlock@users.noreply.github.com>
2024-03-04 22:15:09 +0600

    Update Persistent Segement tree.cpp

--- Data Structures/Persistent Segement tree.cpp
+++ Data Structures/Persistent Segement tree.cpp
@@ -17,9 +17,9 @@ typedef int T;
 //     struct PSEGtree{
         T arr[M];
         int L[M], R[M], root[N];
-        size_t n;
+        size_t sz;
         int nodes = 0, cnt = 0;
-        T temp;
+        T val;
         T (*f)(T, T);
 
         void build(T a[], int v, int tl, int tr){
@@ -31,21 +31,21 @@ typedef int T;
                 arr[v] = f(arr[L[v]], arr[R[v]]);
             }
         }
-        void PSEGtree(T a[], size_t sz, T (*func)(T, T), T val){
-            n = sz, f = func, temp = val;
-            build(a, root[cnt++] = nodes++, 0, n-1);
+        void PSEGtree(T a[], size_t n, T (*func)(T, T), T x){
+            sz = n, f = func, val = x;
+            build(a, root[cnt++] = nodes++, 0, sz-1);
         }
         void reset(){ nodes = cnt = 0; }
 
         T query(int v, int l, int r, int tl, int tr){
-            if (l > tr || r < tl) return temp;
+            if (l > tr || r < tl) return val;
             if (l <= tl && r >= tr) return arr[v];
             int tm = (tl + tr) / 2;
             return f(query(L[v], l, r, tl, tm),
                     query(R[v], l, r, tm+1, tr));
         }
         T query(int l, int r, int rt){
-            return query(root[rt], l, r, 0, n-1);
+            return query(root[rt], l, r, 0, sz-1);
         }
 
         void update(int cur, int prev, int pos, T x, int tl, int tr){
@@ -63,7 +63,7 @@ typedef int T;
         }
         int update(int pos, T x, int rt = -1){
           if (rt < 0) rt = cnt-1;
-          update(root[cnt] = nodes++, root[rt], pos, x, 0, n-1);
+          update(root[cnt] = nodes++, root[rt], pos, x, 0, sz-1);
           return cnt++;
         }
 //     };
