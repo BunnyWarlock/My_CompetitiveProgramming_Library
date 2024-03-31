ee64417962f43082fff6ffd6ef4022493ec1abf5
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-03-31 15:46:58 +0600

    Update Pollard Rho.cpp

--- Algorithms/Math/Pollard Rho.cpp
+++ Algorithms/Math/Pollard Rho.cpp
@@ -39,10 +39,10 @@ bool millerRabin(ull n) {
 ull gcd(ull a, ull b) {
     if (!a || !b)
         return a | b;
-    unsigned shift = __builtin_ctz(a | b);
-    a >>= __builtin_ctz(a);
+    unsigned shift = __builtin_ctzll(a | b);
+    a >>= __builtin_ctzll(a);
     do {
-        b >>= __builtin_ctz(b);
+        b >>= __builtin_ctzll(b);
         if (a > b) swap(a, b);
         b -= a;
     } while (b);
