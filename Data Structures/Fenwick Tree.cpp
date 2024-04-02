bc50232fafdbdfdffd25d9850a99dbebf14b6e0d
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-04-02 23:49:43 +0600

    Update Fenwick Tree.cpp

--- Data Structures/Fenwick Tree.cpp
+++ Data Structures/Fenwick Tree.cpp
@@ -53,19 +53,19 @@ namespace fenwickTree{
 using namespace fenwickTree;
 
 typedef long long ll;
-typedef unsigned long long ull;
 const ll MOD = 998244353;
 
-ull modmul(ull a, ull b, ull M) {
-	ll ret = a * b - M * ull(1.L / M * a * b);
-	return ret + M * (ret < 0) - M * (ret >= (ll)M);
-}
-
-ull modpow(ull b, ull e, ull mod) {
-	ull ans = 1;
-	for (; e; b = modmul(b, b, mod), e /= 2)
-		if (e & 1) ans = modmul(ans, b, mod);
-	return ans;
+ll modpow(ll x, ll p, ll m){
+  ll ans = 1;
+  x %= m;
+  if (!x) return 0;
+  while(p){
+    if (p&1)
+      ans = (ans*x)%m;
+    p = p>>1;
+    x = (x*x)%m;
+  }
+  return ans;
 }
 
 int main(){
