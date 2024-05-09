28c14ce63eadbe62fd7135c7fd0687c0f9441de2
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2024-05-09 22:15:16 +0600

    Create String Hashing.cpp

---
+++ Algorithms/Strings/String Hashing.cpp
@@ -0,0 +1,97 @@
+// Author: Simon Lindholm
+// Tested here:
+// https://www.spoj.com/problems/NHAY/
+// https://open.kattis.com/problems/hashing
+
+#include <iostream>
+#include <sys/time.h>
+#include <vector>
+#include <string>
+using namespace std;
+#define endl '\n'
+
+typedef uint64_t ull;
+static int C; // initialized below
+
+// Arithmetic mod two primes and 2^32 simultaneously.
+// "typedef uint64_t H;" instead if Thue-Morse does not apply.
+template<int M, class B>
+struct A {
+	int x; B b; A(int x = 0): x(x), b(x) {}
+	A(int x, B b): x(x), b(b) {}
+	A operator+(A o){ int y = x+o.x; return{ y - (y>=M)*M, b+o.b }; }
+	A operator-(A o){ int y = x-o.x; return{ y + (y< 0)*M, b-o.b }; }
+	A operator*(A o) { return { (int)(1LL*x*o.x % M), b*o.b }; }
+	explicit operator ull() { return x ^ (ull) b << 21; }
+	bool operator==(A o) const { return (ull)*this == (ull)o; }
+	bool operator<(A o) const { return (ull)*this < (ull)o; }
+};
+typedef A<1000000007, A<1000000009, unsigned>> H;
+
+struct HashInterval {
+	vector<H> ha, pw;
+	HashInterval(string& str) : ha(str.size()+1), pw(ha) {
+		pw[0] = 1;
+		for (int i = 0; i < str.size(); ++i)
+			ha[i+1] = ha[i] * C + str[i],
+			pw[i+1] = pw[i] * C;
+	}
+	H hashInterval(int a, int b) { // hash [a, b)
+		return ha[b] - ha[a] * pw[b - a];
+	}
+};
+
+// Get hash of all substring in str of a specific length
+vector<H> getHashes(string& str, int length) {
+	if (str.size() < length) return {};
+	H h = 0, pw = 1;
+	for (int i = 0; i < length; ++i)
+		h = h * C + str[i], pw = pw * C;
+	vector<H> ret = {h};
+	for (int i = length; i < str.size(); ++i)
+		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
+	return ret;
+}
+
+H hashString(string& s){
+	H h{};
+	for(char c: s)
+		h = h*C + c;
+	return h;
+}
+
+void init(){
+	timeval tp;
+	gettimeofday(&tp, 0);
+	C = (int)tp.tv_usec; // (less than modulo)
+	// assert((ull)(H(1)*2+1-3) == 0);
+}
+
+int main(){
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+
+	init();
+	/*int n, i;
+	string p, t; // patern, text
+	while(cin>>n>>p>>t){
+		vector<H> hashes = getHashes(t, n);
+		ull h = (ull)hashString(p);
+		for (i = 0; i < hashes.size(); ++i)
+			if ((ull)hashes[i] == h)
+				cout<<i<<endl;
+		cout<<endl;
+	}*/
+
+	int q, l, r;
+	string s;
+	cin>>s;
+	HashInterval hash(s);
+	cin>>q;
+	while(q--){
+		cin>>l>>r;
+		cout<<(ull)hash.hashInterval(l, r)<<endl;
+	}
+
+    return 0;
+}
