f1a3baa5eb064de760b8045b59c1692289194a7e
Sahil Yasar <93790489+BunnyWarlock@users.noreply.github.com>
2025-03-10 01:36:17 +0600

    Update Z Adjoined SqrtX.cpp

--- Algorithms/Miscellaneous/Z Adjoined SqrtX.cpp
+++ Algorithms/Miscellaneous/Z Adjoined SqrtX.cpp
@@ -1,14 +1,22 @@
 // Author: Sahil Yasar
 // Z adjoined sqrt(X) or Z[sqrt(X)] struct
 // Tested here:
-// https://cses.fi/problemset/task/1722
+// https://dmoj.ca/problem/fibonacci2
 
 #include <iostream>
 #include <cassert>
+#include <bitset>
+#include <string>
+#include <vector>
+#include <iomanip>
 using namespace std;
 #define endl '\n'
 
-namespace Z_sqrtX{
+const int MAXB = 4e5;
+typedef long long ll;
+typedef unsigned long long ull;
+
+namespace rootNumberSystem{
 	template <class T, int X>
 	struct rootX{
 		T a, b; // a + b*sqrt(X)
@@ -26,10 +34,18 @@ namespace Z_sqrtX{
 			return (*this * z.conj()) / (z.a*z.a - z.b*z.b*X);
 		}
 	    inline rx operator/(T z) { return rx(a/z, b/z); }
-		inline rx operator^(int64_t e) {
+		inline rx operator^(ll e) {
 			if (!e) return rx(1);
 			rx z = *this ^ (e / 2); z = z * z;
 			return e&1 ? *this * z : z;
+		}
+		inline rx operator^(bitset<MAXB>& b){
+			rx ans(1); rx z = *this;
+			for (int i = 0; i < MAXB; ++i){
+				if (b[i]) ans = ans * z;
+				z = z * z;
+			}
+			return ans;
 		}
 		inline rx conj() { return rx(a, b*-1); }
 	};
@@ -38,54 +54,172 @@ namespace Z_sqrtX{
 		return out<<z.a<<" + "<<z.b<<"sqrt("<<X<<") ";
 	}
 }
-using namespace Z_sqrtX;
+using namespace rootNumberSystem;
 
 namespace modInt{
-    int64_t extgcd(int64_t a, int64_t b, int64_t& x, int64_t& y) {
+    ll extgcd(ll a, ll b, ll& x, ll& y) {
         if (!b){ x = 1, y = 0; return a; }
-        int64_t res = extgcd(b, a%b, y, x);
+        ll res = extgcd(b, a%b, y, x);
         y -= (a/b)*x;
         return res;
     }
 
-    template <const int64_t MOD>
+    template <const ll MOD>
     struct mint{
-        int64_t val;
+        ll val;
         using Mint = mint<MOD>;
         mint(): val(0) {}
-        mint(int64_t x) { val = ((x % MOD) + MOD) % MOD; }
+        mint(ll x) { val = ((x % MOD) + MOD) % MOD; }
         inline Mint operator=(Mint b) { val = b.val; return *this; }
-        inline Mint operator=(int64_t b) { return *this = Mint(b); }
+        inline Mint operator=(ll b) { return *this = Mint(b); }
     	inline Mint operator+(Mint b) { return Mint(val + b.val); }
-        inline Mint operator+(int64_t b) { return Mint(val + b); }
+        inline Mint operator+(ll b) { return Mint(val + b); }
     	inline Mint operator-(Mint b) { return Mint(val - b.val); }
-        inline Mint operator-(int64_t b) { return Mint(val - b); }
+        inline Mint operator-(ll b) { return Mint(val - b); }
     	inline Mint operator*(Mint b) { return Mint(val * b.val); }
-        inline Mint operator*(int64_t b) { return Mint(val * b); }
+        inline Mint operator*(ll b) { return Mint(val * b); }
     	inline Mint operator/(Mint b) { return *this * b.invert(); }
-        inline Mint operator/(int64_t b) { return *this * Mint(b).invert(); }
+        inline Mint operator/(ll b) { return *this * Mint(b).invert(); }
     	Mint invert() {
-    		int64_t x, y, g = extgcd(val, MOD, x, y);
+    		ll x, y, g = extgcd(val, MOD, x, y);
     		assert(g == 1); return Mint(x);
     	}
-    	inline Mint operator^(int64_t e) {
+    	inline Mint operator^(ll e) {
     		if (!e) return Mint(1);
     		Mint r = *this ^ (e / 2); r = r * r;
     		return e&1 ? *this * r : r;
     	}
     };
 
-    template <int64_t MOD> istream& operator>>(istream& in, mint<MOD>& n) { return in>>n.val; }
-    template <int64_t MOD> ostream& operator<<(ostream& out, mint<MOD> n) { return out<<n.val; }
+    template <ll MOD> istream& operator>>(istream& in, mint<MOD>& n) { return in>>n.val; }
+    template <ll MOD> ostream& operator<<(ostream& out, mint<MOD> n) { return out<<n.val; }
 }
 using namespace modInt;
+
+namespace bigInt{
+	typedef vector<ll> lnum;
+	const ll base = 1e18;
+
+	void print(lnum& a){
+	    cout<<(a.empty()?0:a.back());
+	    for (int i=(int)a.size()-2; i>=0; --i)
+	        cout<<setfill('0')<<setw(18)<<a[i];
+	    cout<<endl;
+	}
+
+	lnum get(string& s){
+	    lnum a;
+	    for (int i=(int)s.length(); i>0; i-=18){
+	        if (i < 18)
+	            a.push_back (stoll(s.substr (0, i).c_str()));
+	        else
+	            a.push_back (stoll(s.substr (i-18, 18).c_str()));
+	    }
+	    while (a.size() > 1 && a.back() == 0)
+	        a.pop_back();
+	    return a;
+	}
+
+	lnum operator+(lnum a, const lnum& b){
+	    ll carry = 0;
+	    for (size_t i=0; i<max(a.size(),b.size()) || carry; ++i) {
+	        if (i == a.size())
+	            a.push_back (0);
+	        a[i] += carry + (i < b.size() ? b[i] : 0);
+	        carry = a[i] >= base;
+	        if (carry)  a[i] -= base;
+	    }
+	    return a;
+	}
+
+	lnum operator-(lnum a, const lnum& b){
+		ll carry = 0;
+		for (size_t i=0; i < b.size() || carry; ++i) {
+		    a[i] -= carry + (i < b.size() ? b[i] : 0);
+		    carry = a[i] < 0;
+		    if (carry)  a[i] += base;
+		}
+		while (a.size() > 1 && a.back() == 0)
+		    a.pop_back();
+		return a;
+	}
+
+	lnum operator*(lnum a, const lnum& b){
+	    lnum c (a.size()+b.size());
+	    for (size_t i=0; i < a.size(); ++i)
+	        for (ll j=0, carry=0; j<(int)b.size() || carry; ++j) {
+	            __int128_t cur = c[i+j] + (__int128_t)a[i] * (j < (int)b.size() ? b[j] : 0) + carry;
+	            c[i+j] = ll(cur % base);
+	            carry = ll(cur / base);
+	        }
+	    while (c.size() > 1 && c.back() == 0)
+	        c.pop_back();
+	    return c;
+	}
+
+	lnum operator*(lnum a, const ll b){
+	    ll carry = 0;
+	    for (size_t i=0; i < a.size() || carry; ++i) {
+	        if (i == a.size())
+	            a.push_back (0);
+	        __int128_t cur = carry + (__int128_t)a[i] * b;
+	        a[i] = ll(cur % base);
+	        carry = ll(cur / base);
+	    }
+	    while (a.size() > 1 && a.back() == 0)
+	        a.pop_back();
+	    return a;
+	}
+
+	lnum operator/(lnum a, const ll b){
+	    ll carry = 0;
+	    for (int i=(int)a.size()-1; i>=0; --i) {
+	        __int128_t cur = a[i] + (__int128_t)carry * base;
+	        a[i] = ll(cur / b);
+	        carry = ll(cur % b);
+	    }
+	    while (a.size() > 1 && a.back() == 0)
+	        a.pop_back();
+	    return a;
+	}
+
+	ull operator%(lnum& a, const ull m){
+		__uint128_t ret = 0;
+		for (int i = (int)a.size()-1; i >= 0; --i)
+			ret = (ret*base + a[i]) % m;
+		return (ull)ret;
+	}
+
+	bool isEven(lnum& a){
+		if (a.empty()) return false;
+		return a[0]%2 == 0;
+	}
+
+	bitset<MAXB> toBits(lnum a){
+		bitset<MAXB> ret;
+		int i = 0;
+		while(!a.empty() && !(a.size() == 1 && a[0] == 0)){
+			ull temp = a%((ull)0x1000000000000000);
+			for (int j = 0; j < 60; ++j, ++i)
+				ret[i] = (temp>>j)&1;
+			a = a/((ll)0x1000000000000000);
+		}
+		return ret;
+	}
+};
+using namespace bigInt;
 
 const int MOD = 1e9 + 7;
 typedef rootX<mint<MOD>, 5> r5;
 mint<MOD> half = mint<MOD>(2).invert();
 mint<MOD> fifth = mint<MOD>(5).invert();
 
-int64_t fib(int64_t n){
+ll fib(ll n){
+	r5 f = (r5(half, half)^n) - (r5(half, half*-1)^n);
+	f = f*r5(0, fifth);
+	return f.a.val;
+}
+ll fib(bitset<MAXB> n){
 	r5 f = (r5(half, half)^n) - (r5(half, half*-1)^n);
 	f = f*r5(0, fifth);
 	return f.a.val;
@@ -95,9 +229,10 @@ int main(){
     cin.tie(0)->sync_with_stdio(0);
     cin.exceptions(cin.failbit);
 
-	int64_t n;
-    cin>>n;
-    cout<<fib(n)<<endl;
+	string s;
+	cin>>s;
+	bitset<MAXB> b = toBits(get(s));
+	cout<<fib(b)<<endl;
 
     return 0;
 }
